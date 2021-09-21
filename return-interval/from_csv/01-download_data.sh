206c79a0ad5749c09121124e01ec4b17be7cba70
DesertLabRat <63479684+DesertLabRat@users.noreply.github.com>
2021-09-21 14:25:21 -0700

    Update 01-download_data.sh
    modify for generic data downloading

--- return-interval/from_csv/01-download_data.sh
+++ return-interval/from_csv/01-download_data.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-DLSTART=202101010000
-DLEND=202108152359
+DLSTART=199701010000 #start date for data download, format %Y%m%d%H%M
+DLEND=202108310000 #end date for data download, format %Y%m%d%H%M
 
-VARS='air_temp,relative_humidity,wind_speed,wind_direction,wind_gust,precip_accum'
+VARS='air_temp,relative_humidity,wind_speed,wind_direction,wind_gust,precip_accum' #variables to be downloaded
 
-STIDS='NV019' #2021 FMS
-#COUNTY='chelan'
-#STATE='wa'
+STIDS='SMDC1 HMDC1' #list of station IDs to be donwloaded, example: Stampede and Homewood RAWS
 
-rm -r -f NVE_081721
-mkdir NVE_081721 2> /dev/null
+rm -r -f YOUR_DIRECTORY_HERE #remove previously downloaded directory and contents
+mkdir YOUR_DIRECTORY_HERE 2> /dev/null #create directory for api request downloads
 
 for STID in $STIDS; do
-   wget -O ./NVE_081721/$STID.csv "https://api.mesowest.net/v2/stations/timeseries?stid=$STID&start=$DLSTART&end=$DLEND&vars=$VARS&timeformat=%Y-%m-%d_%H:%M&output=csv&units=speed|mph,precip|in,english&qc_flags=off&token=74df9291a38448fdbedcd394ed889b24&"
-#   wget -O test_CA.csv  "https://api.mesowest.net/v2/stations/timeseries?state=ca&start=$DLSTART&end=$DLEND&vars=$VARS&timeformat=%Y-%m-%d_%H:%M&output=csv&units=speed|mph,precip|in,english&qc_flags=off&token=74df9291a38448fdbedcd394ed889b24&"
+   wget -O ./YOUR_DIRECTORY_HERE/$STID.csv "https://api.mesowest.net/v2/stations/timeseries?stid=$STID&start=$DLSTART&end=$DLEND&vars=$VARS&timeformat=%Y-%m-%d_%H:%M&output=csv&units=speed|mph,precip|in,english&qc_flags=off&token=74df9291a38448fdbedcd394ed889b24&"
+   #see Synoptic Labs for api request options: https://developers.synopticdata.com/mesonet/
 done
 
 exit 0
